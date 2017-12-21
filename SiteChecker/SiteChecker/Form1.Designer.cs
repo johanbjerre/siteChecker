c8a2c21ad3032297140bb1803bbb2579d656a2e1
Johan Bjerre <johan.bjerre@ist.com>
2017-12-21 18:22:39 +0100

    first

---
+++ SiteChecker/SiteChecker/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SiteChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this._log = new System.Windows.Forms.RichTextBox();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxRepeatTime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _log
+            // 
+            this._log.Location = new System.Drawing.Point(102, 70);
+            this._log.Name = "_log";
+            this._log.Size = new System.Drawing.Size(705, 450);
+            this._log.TabIndex = 1;
+            this._log.Text = "";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(171, 15);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(334, 20);
+            this.textBoxUrl.TabIndex = 2;
+            this.textBoxUrl.Text = "http://www.dn.se";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "url";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(12, 41);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxRepeatTime
+            // 
+            this.textBoxRepeatTime.Location = new System.Drawing.Point(171, 44);
+            this.textBoxRepeatTime.Name = "textBoxRepeatTime";
+            this.textBoxRepeatTime.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRepeatTime.TabIndex = 5;
+            this.textBoxRepeatTime.Text = "100";
+            this.textBoxRepeatTime.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "sleep";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 532);
+            this.Controls.Add(this.textBoxRepeatTime);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this._log);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.RichTextBox _log;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBoxRepeatTime;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SiteChecker/SiteChecker/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiteChecker
+{
+    public partial class Form1 : Form
+    {
+        public Boolean shallRepeat = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            shallRepeat = true;
+            AddLog("Start");
+            
+            var waitTime = Int32.Parse(textBoxRepeatTime.Text);
+            
+            
+
+            new Thread(() =>
+            {
+                while (shallRepeat)
+                {
+                    repeattask();
+                    Thread.Sleep(waitTime);
+                    AddLog("waited "+ waitTime);
+                }
+            }).Start();
+            AddLog("Stop");
+            
+
+        }
+
+        private void repeattask()
+        {
+            AddLog("repeattask");
+            var scraper = new Bjerre.Scraper.Scraper();
+            var content=scraper.PerformRequest(textBoxUrl.Text);
+            AddLog("found content of size:"+content.Length);
+        }
+
+        private delegate void AddLogDelegate(string text);
+        private void AddLog(string text)
+        {
+            if (_log.InvokeRequired)
+            {
+                var addLogDelegate = new AddLogDelegate(AddLog);
+                Invoke(addLogDelegate, new object[] { text });
+            }
+            else
+            {
+                _log.Text += text + Environment.NewLine;
+                _log.Select(_log.Text.Length, 0);
+                _log.ScrollToCaret();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            shallRepeat = false;
+        }
+    }
+}
